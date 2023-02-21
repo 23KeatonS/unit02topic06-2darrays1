a50e7993e385cd20b2c1f3d08bac93aaf5c387f3
Keaton Stwalley <23keatonstwalley@usd266.com>
2023-02-21 11:52:45 -0600

    finished withWeightedAverages, feels kinds messy

--- Main.java
+++ Main.java
@@ -53,6 +53,40 @@ public class Main {
   }
 
 
+
+  public static double[][] withWeightedAverage(double[][] a, double[] weights){
+    int rows = a.length;
+    int columns = a[0].length;
+    int retRows = a.length+1;
+    int retColumns = a[0].length+1;
+    double [][] retVal = new double[retRows][retColumns];
+    for (int r = 0; r<rows;r++){
+      for (int c = 0; c<columns;c++){
+        retVal[r][c] = a[r][c];
+      }
+    }
+     
+    for(int r = 0;r<rows;r++){
+      double weightedAvg = 0;
+      for(int c = 0;c<columns;c++){
+        weightedAvg += (retVal[r][c]*weights[c]);
+      }
+      retVal[r][columns] = weightedAvg;
+    }
+
+    for (int c = 0; c<retColumns;c++){
+      double sum = 0;
+      for (int r = 0; r<rows;r++){
+        sum+= retVal[r][c];
+      }
+      retVal[rows][c] = sum/rows;
+    }
+    return retVal;
+  }
+  
+
+
+
   public static void main(String[] args) {
     int [][] bruh = new int[][] {{2,3,4,5},{4,5,6,7},{2,3,5,6},{2,3,4,1}};
     System.out.println(Arrays.deepToString(CopySquare(bruh)));
@@ -61,7 +95,23 @@ public class Main {
     int [][] bruh3 = new int[][] {{2,3,5},{4,5,6,7},{2,3,5,6},{2,3,4,1,5}};
     System.out.println(Arrays.deepToString(CopyRugged(bruh3)));
     System.out.println(Arrays.deepToString(CopyRugged(bruh)));
-    printTranspose(bruh2);
+    printTranspose(bruh);
+    double[][] bruh4 = new double[][] {
+      {99, 85, 98},
+      {98, 57, 79},
+      {92, 77, 74},
+      {94, 62, 81},
+      {99, 94, 92},
+      {80, 76.5, 67},
+      {76, 58.5, 90.5},
+      {92, 66, 91},
+      {77, 70.5, 66.5},
+      {89, 89.5, 81}};
+    double[] bruh5 = new double[] {0.25,0.25,0.5};
+    double[][] bruh6 = (withWeightedAverage(bruh4,bruh5));
+    for (double[] row:bruh6){
+      System.out.println(Arrays.toString(row));
+    }
     
   }
 }
